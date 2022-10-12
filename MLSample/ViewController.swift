0603f953fe8e6a71d552793248524fe392df819f
hasanuysaal <hsnuysaal@icloud.com>
2022-10-12 18:09:02 +0300

    requesting process is finished

--- MLSample/ViewController.swift
+++ MLSample/ViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    var chosenImage = CIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +33,45 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true)
+        
+        if let ciImage = CIImage(image: imageView.image!){
+            
+            self.chosenImage = ciImage
+            
+        }
+        
+        recognizeImage(image: self.chosenImage)
+    }
+    
+    func recognizeImage(image: CIImage){
+        
+        resultLabel.text = "Finding ... "
+        
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
+            
+            let request = VNCoreMLRequest(model: model) { vnRequest, error in
+                
+                if let results = vnRequest.results as? [VNClassificationObservation] {
+                    if results.count > 0 {
+                        
+                        let topResult = results.first
+                        
+                        DispatchQueue.main.async {
+                            
+                            let confidenceLevel = (topResult?.confidence ?? 0) * 100
+                            
+                            let rounded = Int(confidenceLevel * 100) / 100
+                            
+                            self.resultLabel.text = "\(rounded)  \(topResult?.identifier)"
+                        }
+                    }
+                }
+            }
+        }
+        
     }
     
 }
