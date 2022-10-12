a7250ce8bbfbaf4eddbee84e7988c615e01762fd
hasanuysaal <hsnuysaal@icloud.com>
2022-10-12 17:37:23 +0300

    Initial Commit

---
+++ MLSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLSample
+//
+//  Created by Hasan Uysal on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
